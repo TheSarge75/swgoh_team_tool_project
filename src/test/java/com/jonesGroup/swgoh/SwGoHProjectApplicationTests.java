f89052060247617366cd3eaf639e3787cd65cfe1
TheSarge75 <joshua.a.jones.75@gmail.com>
2020-08-07 17:23:13 -0500

    Adds org.JSoup controls that return and parse the HTML for the login page; Adds some basic unit tests to ensure the files are found and parsed; Renames login.html to lightside.html;

--- src/test/java/com/jonesGroup/swgoh/SwGoHProjectApplicationTests.java
+++ src/test/java/com/jonesGroup/swgoh/SwGoHProjectApplicationTests.java
@@ -1,13 +1,41 @@
 package com.jonesGroup.swgoh;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootTest
 class SwGoHProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static File lightSideFile;
+	private static Document lightSideDocument;
+
+	@BeforeAll
+	private static void setup(){
+		lightSideFile = new File("/Users/josh_1/IdeaProjects/swgohartifact/src/main/resources/static/html/lightside.html");
+		try {
+			lightSideDocument = Jsoup.parse(lightSideFile, "UTF-8");
+		} catch (IOException e) {
+			lightSideDocument = null;
+		}
 	}
+
+	@Test
+	void itShouldExist(){
+		Assertions.assertTrue(lightSideFile.exists());
+	}
+
+	@Test
+	void itShouldNotBeNull(){
+		Assertions.assertNotNull(lightSideDocument);
+	}
+
+
 
 }
